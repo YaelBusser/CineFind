f982c4dc900af49799dcc5c3eb81a1b1253a4c90
Yaël Busser <mrmaclevine@gmail.com>
2023-02-20 17:20:57 +0100

    add project

---
+++ src/components/MovieList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div v-for="movie in movies">
+      {{ movie.title }}
+    </div>
+  </div>
+</template>
+<script>
+import process from 'process'
+export default {
+  data() {
+    return {
+      movies: []
+    }
+  },
+  methods: {
+    async movieListPop() {
+      fetch(`https://api.themoviedb.org/3/movie/popular?api_key=9f49de7ae4e7847f4cd272851ed07488&language=fr&page=1`)
+          .then(response => response.json())
+          .then(data => {
+            this.movies = data.results
+          }).catch(error => {
+        console.log(error);
+      });
+    }
+  },
+  mounted() {
+    this.movieListPop();
+  }
+}
+</script>
+<style scoped>
+
+</style>
