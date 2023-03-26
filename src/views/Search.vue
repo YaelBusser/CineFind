0fb7f7530de2a7c0e7d27175f6d3fcb02857fa09
Yaël Busser <mrmaclevine@gmail.com>
2023-03-27 01:51:22 +0200

    ok

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,16 +1,29 @@
 <template>
+  <details-by-id :path="path" />
   <div class="body-search">
     <div class="block-movie">
-      <div v-for="resultMovie in resultsMovie">
-        <div class="itemResult" v-if="result !== null">
-          <img :src="`https://image.tmdb.org/t/p/w500/${resultMovie.poster_path}`">
+      <h2>Films</h2>
+      <div class="movies">
+        <div v-for="(resultMovie, index) in resultsMovie" class="block-item">
+          <div class="itemResult" v-if="result !== null">
+            <img :src="`https://image.tmdb.org/t/p/w500/${resultMovie.poster_path}`" @mouseover="toggleCard(); hoverId = index">
+          </div>
+          <div class="card" :style="showCard === true && hoverId === index ? 'opacity: 1; z-index: 2' : 'opacity: 0; z-index: -1' " @mouseleave="showCard = false">
+            <div class="block-btn" @click="showInfos; idCard = resultsMovie.id">
+              <i class="fa-solid fa-circle-info"></i>
+              <p>Plus d'infos</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
     <div class="block-tv">
-      <div v-for="resultTv in resultsTv">
-        <div class="itemResult" v-if="result !== null">
-          <img :src="`https://image.tmdb.org/t/p/w500/${resultTv.poster_path}`">
+      <h2>Séries</h2>
+      <div class="tv">
+        <div v-for="resultTv in resultsTv">
+          <div class="itemResult" v-if="result !== null">
+            <img :src="`https://image.tmdb.org/t/p/w500/${resultTv.poster_path}`">
+          </div>
         </div>
       </div>
     </div>
@@ -18,13 +31,20 @@
 </template>
 
 <script>
+import DetailsById from "../components/detailsById.vue";
+
 export default {
   name: "Search",
+  components: {DetailsById},
   data() {
     return {
       resultsMovie: [],
       resultsTv: [],
       i: 0,
+      hoverId: 0,
+      showCard: false,
+      idCard: 0,
+      path: "/search",
     }
   },
   watch: {
@@ -34,10 +54,17 @@ export default {
     }
   },
   methods: {
-    redirect() {
-      if (this.$route.query.q === null) {
-        this.$router.push({path: '/'});
-      }
+    cardMovie() {
+      this.$router.push({path: this.$route.path, query: {details: this.idCard}});
+    },
+    cardSerie() {
+      this.$router.push({path: this.$route.path, query: {detailsSerie: this.idCard}});
+    },
+    showInfos() {
+      this.$emit('card-infos-little', this.idCard);
+    },
+    toggleCard(){
+      this.showCard = true;
     },
     searchResults() {
       if (this.$route.query.q === "") {
@@ -65,33 +92,54 @@ export default {
 </script>
 
 <style scoped>
+.card{
+  width: 24vw;
+  height: 45vh;
+  position: absolute;
+  top: -6vw;
+  left: -5vw;
+  border-radius: 10px 10px 10px 10px;
+  overflow: hidden;
+  background-color: #1a1a1a;
+  box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2);
+  opacity: 1;
+  z-index: -1;
+  transition: all ease-in-out 0.3s;
+}
 .body-search {
   margin-top: 15vh;
   display: flex;
   flex-direction: column;
   gap: 5vh;
+  margin-left: 5vw;
 }
-
+h2{
+  font-size: 1.5vw;
+}
 img {
   width: 100%;
   height: 100%;
   object-fit: cover;
+  border-radius: 0.3vw;
+  z-index: 1;
 }
 
 .itemResult {
-  width: 20vw;
-  height: 25vh;
+  width: 15vw;
+  height: 20vh;
 }
 
-.block-movie {
+.block-tv, .block-movie {
   width: 100%;
   display: flex;
+  flex-direction: column;
   gap: 1vw;
 }
-
-.block-tv {
-  width: 100%;
+.movies, .tv{
   display: flex;
-  gap: 1vw;
+  gap: 0.5vw;
+}
+.block-item{
+  position: relative;
 }
 </style>
